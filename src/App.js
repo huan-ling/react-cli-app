ad52a24e14d8d7b8c8543bb24de16ba4377fb8cc
wb_xugz <xgz1112@outlook.com>
2019-07-18 16:13:31 +0800

    树形组件

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import request from 'superagent';
-import { Button,Row, Col,Icon,Tabs } from 'antd';
-
-const TabPane = Tabs.TabPane;
+//import request from 'superagent';
+import { Row, Col } from 'antd';
+import Title from './component/Title';
+import Tab from './component/Tab';
 
 class App extends React.Component {
     constructor(props) {
@@ -14,24 +14,19 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        this.serverRequest = request
-            .get(this.props.source)
-            .query({msg : "HI"})
-            .then(response =>{
-                this.setState({
-                    msg : response.body.data
-                });
-            })
+        // this.serverRequest = request
+        //     .get(this.props.source)
+        //     .query({msg : "HI"})
+        //     .then(response =>{
+        //         this.setState({
+        //             msg : response.body.data
+        //         });
+        //     })
     }
 
 
     componentWillUnmount() {
-        this.serverRequest.abort();
-    }
-
-
-    callback(key) {
-        console.log(key);
+        // this.serverRequest.abort();
     }
 
 
@@ -41,23 +36,14 @@ class App extends React.Component {
                 <Row style={{marginTop:10}}>
                     <Col span={4}></Col>
                     <Col span={16}>
-                        <Icon type="bars" />
-                        <h3 style={{display : "inline-block", marginLeft:10}}>标题</h3>
-                        <div style={{display: "inline-block", float:"right"}}>
-                            <a target={"_blank"} href={"https://1x.ant.design/components/tabs/"}>antd组件</a>
-                        </div>
+                        <Title/>
                     </Col>
                     <Col span={4}></Col>
                 </Row>
                 <Row style={{marginTop:20}}>
                     <Col span={4}></Col>
                     <Col span={26}>
-                        <Tabs defaultActiveKey="1" onChange={this.callback()}>
-                            <TabPane tab="选项卡一" key="1">选项卡一内容</TabPane>
-                            <TabPane tab="选项卡二" key="2">选项卡二内容</TabPane>
-                            <TabPane tab="选项卡三" key="3">选项卡三内容</TabPane>
-                        </Tabs>
-
+                        <Tab></Tab>
                     </Col>
                     <Col span={4}></Col>
                 </Row>
